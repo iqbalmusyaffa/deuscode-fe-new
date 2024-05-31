79aeee025ecc161f3bfa72b3f96d09646b5a1c75
iqbalmusyaffa <yusa82401@gmail.com>
2024-05-31 16:45:42 +0700

    Update commit

---
+++ src/router/index.ts
@@ -0,0 +1,155 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import SigninView from '@/views/Authentication/SigninView.vue'
+import SignupView from '@/views/Authentication/SignupView.vue'
+// import CalendarView from '@/views/CalendarView.vue'
+import BasicChartView from '@/views/Charts/BasicChartView.vue'
+import ECommerceView from '@/views/Dashboard/ECommerceView.vue'
+import FormElementsView from '@/views/Forms/FormElementsView.vue'
+import FormLayoutView from '@/views/Forms/FormLayoutView.vue'
+import SettingsView from '@/views/Pages/SettingsView.vue'
+import ProfileView from '@/views/ProfileView.vue'
+import TablesView from '@/views/TablesView.vue'
+import AlertsView from '@/views/UiElements/AlertsView.vue'
+import ButtonsView from '@/views/UiElements/ButtonsView.vue'
+import DatapenggunaView from '@/views/DatapenggunaView.vue'
+import Loginpage from '@/views/Authentication/Loginpage.vue'
+import DataprodukView from '@/views/DataprodukView.vue'
+
+const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: Loginpage,
+    meta: {
+      title: 'login'
+    }
+  },
+  {
+    path: '/',
+    name: 'dashboard',
+    component: ECommerceView,
+    meta: {
+      title: 'Dashboard'
+    }
+  },
+  {
+    path: '/datapengguna',
+    name: 'datapengguna',
+    component: DatapenggunaView,
+    meta: {
+      title: 'datapengguna'
+    }
+  },
+  {
+    path: '/dataproduk',
+    name: 'dataproduk',
+    component: DataprodukView,
+    meta: {
+      title: 'dataproduk'
+    }
+  },
+  {
+    path: '/forms/form-elements',
+    name: 'formElements',
+    component: FormElementsView,
+    meta: {
+      title: 'Form Elements'
+    }
+  },
+  {
+    path: '/forms/form-layout',
+    name: 'formLayout',
+    component: FormLayoutView,
+    meta: {
+      title: 'Form Layout'
+    }
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: DataprodukView,
+    meta: {
+      title: 'Profile'
+    }
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: ProfileView,
+    meta: {
+      title: 'Profile'
+    }
+  },
+  {
+    path: '/tables',
+    name: 'tables',
+    component: TablesView,
+    meta: {
+      title: 'Tables'
+    }
+  },
+  {
+    path: '/pages/settings',
+    name: 'settings',
+    component: SettingsView,
+    meta: {
+      title: 'Settings'
+    }
+  },
+  {
+    path: '/charts/basic-chart',
+    name: 'basicChart',
+    component: BasicChartView,
+    meta: {
+      title: 'Basic Chart'
+    }
+  },
+  {
+    path: '/ui-elements/alerts',
+    name: 'alerts',
+    component: AlertsView,
+    meta: {
+      title: 'Alerts'
+    }
+  },
+  {
+    path: '/ui-elements/buttons',
+    name: 'buttons',
+    component: ButtonsView,
+    meta: {
+      title: 'Buttons'
+    }
+  },
+  {
+    path: '/auth/signin',
+    name: 'signin',
+    component: SigninView,
+    meta: {
+      title: 'Signin'
+    }
+  },
+  {
+    path: '/auth/signup',
+    name: 'signup',
+    component: SignupView,
+    meta: {
+      title: 'Signup'
+    }
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    return savedPosition || { left: 0, top: 0 }
+  }
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = `Vue.js ${to.meta.title} | TailAdmin - Vue.js Tailwind CSS Dashboard Template`
+  next()
+})
+
+export default router
